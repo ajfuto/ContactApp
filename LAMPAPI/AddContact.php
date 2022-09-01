171de18cae6478f290f88ef1b646a249ae477868
Jason Saini <jasonsaini.exe@gmail.com>
2022-09-01 12:20:59 -0400

    Finished add contact endpoint

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,4 +1,42 @@
 <?php
+    $userID = $inData["UserID"];
+    $inData = getRequestInfo();
+    $firstName = $inData["FirstName"];
+    $lastName = $inData["LastName"];
+    $email = $inData["Email"];
+    $phoneNumber = $inData["PhoneNumber"];
+    
 
+    $conn = new mysqli("%", "hackerman", "WeWillGet100", "COP4331");
+    if($conn->connect_error)
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        $stmt = $conn->prepare("INSERT into Contacts (userID, FirstName, LastName, Email, PhoneNumber) VALUES (?,?,?,?,?)");
+        $stmt->bind_param("sssss", $userID, $firstName, $lastName, $email, $phoneNumber);
+        $stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+    } 
 
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+    
+    function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>
