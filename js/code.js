36ed0ac20ef3bba3bcc559f973034c503b9e0b29
zpackattack <treyz3@icloud.com>
2022-09-05 00:24:33 +0000

    Just testing

--- js/code.js
+++ js/code.js
@@ -96,7 +96,7 @@ function readCookie()
 	}
 	else
 	{
-		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+		document.getElementById("userName").innerHTML = "hello " + firstName + " " + lastName;
 	}
 }
 
