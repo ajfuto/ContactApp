1fddf00485b0f93534023753856b510e96f58d6f
raidnaeem <90997042+raidnaeem@users.noreply.github.com>
2022-09-07 18:21:06 -0400

    Update Registration.php
    Registration endpoint - create variables for User fields and create new User

--- LAMPAPI/Registration.php
+++ LAMPAPI/Registration.php
@@ -1,4 +1,53 @@
 <?php
+    
+  $inData = getRequestInfo();
 
+  $firstName = $inData["FirstName"];
+  $lastName = $inData["LastName"];
+  $login = $inData["Login"];
+  $password = $inData["Password"];
+    
+    # open our secrets file
+	$secrets_file = fopen("../secrets/creds.txt", "r") or die("unable to open credentials file");
 
+	# fgets() reads an entire line from our file, rtrim() strips any new line characters
+	$mysql_user = rtrim(fgets($secrets_file));
+	$mysql_pass = rtrim(fgets($secrets_file));
+
+	# close our secrets file
+	fclose($secrets_file);
+
+	$conn = new mysqli("localhost", $mysql_user, $mysql_pass, "COP4331"); 	
+    
+  if($conn->connect_error)
+  {
+      returnWithError( $conn->connect_error );
+  }
+  else
+  {
+      $stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES (?,?,?,?)");
+      $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+      $stmt->execute();
+      $stmt->close();
+      $conn->close();
+      returnWithError("");
+  } 
+
+  function getRequestInfo()
+	{
+	  return json_decode(file_get_contents('php://input'), true);
+	}
+    
+  function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>
