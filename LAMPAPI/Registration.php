4f62d721e4918345c851621daf03b3e36f36de9e
raidnaeem <naeem.raid@gmail.com>
2022-09-15 06:44:23 -0400

    added identical login recognition

--- LAMPAPI/Registration.php
+++ LAMPAPI/Registration.php
@@ -1,51 +1,74 @@
 <?php
 
-$inData = getRequestInfo();
+	$inData = getRequestInfo();
+	$firstName = $inData["FirstName"];
+	$lastName = $inData["LastName"];
+	$login = $inData["Login"];
+	$password = $inData["Password"];
+    
+	# open our secrets file
+	$secrets_file = fopen("../secrets/creds.txt", "r") or die("unable to open credentials file");
 
-$firstName = $inData["FirstName"];
-$lastName = $inData["LastName"];
-$login = $inData["Login"];
-$password = $inData["Password"];
+	# fgets() reads an entire line from our file, rtrim() strips any new line characters
+	$mysql_user = rtrim(fgets($secrets_file));
+	$mysql_pass = rtrim(fgets($secrets_file));
 
-# open our secrets file
-$secrets_file = fopen("../secrets/creds.txt", "r") or die("unable to open credentials file");
+	# close our secrets file
+	fclose($secrets_file);
 
-# fgets() reads an entire line from our file, rtrim() strips any new line characters
-$mysql_user = rtrim(fgets($secrets_file));
-$mysql_pass = rtrim(fgets($secrets_file));
+	$conn = new mysqli("localhost", $mysql_user, $mysql_pass, "COP4331"); 	
+    
+	if($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+    	else if ( !findUser($login) )
+    	{
+		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES (?,?,?,?)");
+		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+    	} 
+	else
+	{
+		returnWithError( "Username taken. Please try again." )
+	}
 
-# close our secrets file
-fclose($secrets_file);
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+    
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
 
-$conn = new mysqli("localhost", $mysql_user, $mysql_pass, "COP4331");
+	function findUser( $login )
+	{
+		$stmt = $conn->prepare("Select * from Users where Login = ?");
+		$stmt->bind_param("s", $login);
+		$stmt->execute();
 
-if ($conn->connect_error) {
-	returnWithError($conn->connect_error);
-} else {
-	$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES (?,?,?,?)");
-	$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
-	$stmt->execute();
-	$stmt->close();
-	$conn->close();
-	returnWithError("");
-}
-
-function getRequestInfo()
-{
-	return json_decode(file_get_contents('php://input'), true);
-}
-
-function sendResultInfoAsJson($obj)
-{
-	header('Content-type: application/json');
-	echo $obj;
-}
-
-function returnWithError($err)
-{
-	$retValue = '{"error":"' . $err . '"}';
-	sendResultInfoAsJson($retValue);
-}
-
+		if( ($result=mysql_query("Select * from Users where Login = $stmt")) )
+		{
+			if(mysql_num_rows($result)) 
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
 	
 ?>
