35d9a346e9446d7860333e8517523bd8f8c6548a
zpackattack <treyz3@icloud.com>
2022-09-25 18:40:33 +0000

    Clickable Email link

--- js/code.js
+++ js/code.js
@@ -50,10 +50,11 @@ function searchOnEnterAdd()
 		addContact();
 	}
 	//hopefully masks phone input
-	document.getElementById('phoneInput').addEventListener('input', function (e) {
+	document.getElementById('phoneInput').addEventListener('input', function (e) 
+	{
 		let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
 		e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-	  });
+	});
 	  
 }
 
@@ -335,7 +336,8 @@ function searchContact()
 					cell.innerHTML = currContact.PhoneNumber;
 
 					cell = row.insertCell();
-					cell.innerHTML = currContact.Email;
+					let currentEmail = currContact.Email;
+					cell.innerHTML = '<a href="mailto:'+currentEmail+'">'+currentEmail+'</a>';
 
 					// row.insertCell(jsonObjects[i]["FirstName"]);
 					// let cell = row.insertCell();
